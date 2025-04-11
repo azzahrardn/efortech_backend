a0588096d98587bc2ca0ed8e96fef9b9414f4cc2
FathimatusZN <fathimatus.zn18@gmail.com>
2025-04-11 15:55:20 +0700

    feat(routes): add manage admin routes

---
+++ routes/manageAdminRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAdmins,
+  getAdminById,
+  createAdmin,
+  updateAdmin,
+  deleteAdmin,
+} = require("../controllers/manageAdminController");
+
+// GET - All Admins (with optional filters)
+router.get("/list", getAdmins);
+
+// GET - Admin by ID
+router.get("/:admin_id", getAdminById);
+
+// POST - Create Admin
+router.post("/create", createAdmin);
+
+// PUT - Update Admin Role
+router.put("/update/:admin_id", updateAdmin);
+
+// DELETE - Soft Delete Admin
+router.delete("/delete/:admin_id", deleteAdmin);
+
+module.exports = router;
