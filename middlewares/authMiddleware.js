8f2cdc2a1e8fb3867326189fc19a69ea68bb7df1
FathimatusZN <fathimatus.zn18@gmail.com>
2025-04-16 14:40:45 +0700

    refactor: refactor authMidlleware using responseUtils for consistent API responses

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,25 @@
 const admin = require("firebase-admin");
 const { getAuth } = require("firebase-admin/auth");
+const {
+  sendBadRequestResponse,
+  sendUnauthorizedResponse,
+} = require("../utils/responseUtils");
 
 const verifyToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "Unauthorized: No token provided" });
+    return sendUnauthorizedResponse(res, "Unauthorized: No token provided");
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
     const decodedToken = await getAuth().verifyIdToken(token);
-    req.user = decodedToken; // Simpan informasi user di req
+    req.user = decodedToken;
     next();
   } catch (error) {
     console.error("Token verification error:", error);
-    return res.status(401).json({ error: "Unauthorized: Invalid token" });
+    return sendUnauthorizedResponse(res, "Unauthorized: No token provided");
   }
 };
 
