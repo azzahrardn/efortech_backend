890cf11c02ab0b1acb9901c6b92d482cf5fd4adc
FathimatusZN <fathimatus.zn18@gmail.com>
2025-04-29 11:40:32 +0700

    feat(certificate): add endpoint for uploading certificate file

--- routes/certificateRoutes.js
+++ routes/certificateRoutes.js
@@ -10,4 +10,18 @@ const {
 // POST /api/certificate - Create a new training certificate
 router.post("/", createCertificate);
 
+// Upload certificate file
+router.post("/upload-certificate", uploadFile, (req, res) => {
+  if (
+    !req.files ||
+    req.files.length === 0 ||
+    !req.files[0].cloudStoragePublicUrl
+  ) {
+    return sendErrorResponse(res, "Failed Upload");
+  }
+  return sendSuccessResponse(res, "Upload successful", {
+    fileUrl: req.files[0].cloudStoragePublicUrl,
+  });
+});
+
 module.exports = router;
