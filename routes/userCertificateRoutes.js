e3e464af83406dad30acad3035552cb3d55e6e5c
FathimatusZN <fathimatus.zn18@gmail.com>
2025-05-08 11:40:07 +0700

    feat(user-certificate): add upload certificate file endpoint

--- routes/userCertificateRoutes.js
+++ routes/userCertificateRoutes.js
@@ -3,8 +3,27 @@ const router = express.Router();
 const {
   createUserCertificate,
 } = require("../controllers/userCertificateController");
+const uploadFile = require("../middlewares/fileUpload");
+const {
+  sendSuccessResponse,
+  sendErrorResponse,
+} = require("../utils/responseUtils");
 
 // POST /api/ucertificate - Create a new user certificate
 router.post("/", createUserCertificate);
+
+// Upload certificate file
+router.post("/upload-ucertificate", uploadFile, (req, res) => {
+  if (
+    !req.files ||
+    req.files.length === 0 ||
+    !req.files[0].cloudStoragePublicUrl
+  ) {
+    return sendErrorResponse(res, "Failed Upload");
+  }
+  return sendSuccessResponse(res, "Upload successful", {
+    fileUrl: req.files[0].cloudStoragePublicUrl,
+  });
+});
 
 module.exports = router;
