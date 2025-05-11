a7147102dd4c4048498a49d20a6443847af2ffe5
FathimatusZN <fathimatus.zn18@gmail.com>
2025-05-11 17:00:29 +0700

    feat(allCertificate): add route for search endpoint

--- routes/allCertificateRoutes.js
+++ routes/allCertificateRoutes.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllCertificates,
+  searchCertificates,
 } = require("../controllers/allCertificateController");
 
 // GET /api/certificates - get all certificates
 router.get("/", getAllCertificates);
+
+// GET /api/certificates/search - get certificates based on query
+router.get("/search", searchCertificates);
 
 module.exports = router;
