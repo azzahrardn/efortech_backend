71985c9aba926f8916ab2ee167c1603d61056594
FathimatusZN <fathimatus.zn18@gmail.com>
2025-05-19 10:07:52 +0700

    feat(email): add route for endpoint send user upload certificate email

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -4,6 +4,7 @@ const {
   previewTrainingCertificateEmail,
   sendTrainingCertificateEmail,
   previewUserUploadCertificateValidationEmail,
+  sendUserUploadCertificateValidationEmail,
 } = require("../controllers/emailController");
 
 // POST /api/email/send-training-certificate- Send Registration Training Certificate Email
@@ -11,6 +12,12 @@ router.post("/send-training-certificate", sendTrainingCertificateEmail);
 
 // POST /api/email/preview-training-certificate - Get email Preview for Registration Training Certificate
 router.post("/preview-training-certificate", previewTrainingCertificateEmail);
+
+// POST /api/email/send-upload-certificate - Send User Upload Certificate Validation Email
+router.post(
+  "/send-upload-certificate",
+  sendUserUploadCertificateValidationEmail
+);
 
 // POST /api/email/preview-upload-certificate - Get email Preview for User Upload Certificate Validation
 router.post(
