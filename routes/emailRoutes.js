356f01ce064504524755d5e2f0959288b5b64bc3
FathimatusZN <fathimatus.zn18@gmail.com>
2025-05-19 10:07:01 +0700

    feat(email): add route for endpoint preview user upload certificate

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const {
   previewTrainingCertificateEmail,
   sendTrainingCertificateEmail,
+  previewUserUploadCertificateValidationEmail,
 } = require("../controllers/emailController");
 
-// POST /api/email/send-certificate - Send Registration Training Certificate
+// POST /api/email/send-training-certificate- Send Registration Training Certificate Email
 router.post("/send-training-certificate", sendTrainingCertificateEmail);
 
-// POST /api/email/preview - Get email Preview for Registration Training Certificate
+// POST /api/email/preview-training-certificate - Get email Preview for Registration Training Certificate
 router.post("/preview-training-certificate", previewTrainingCertificateEmail);
+
+// POST /api/email/preview-upload-certificate - Get email Preview for User Upload Certificate Validation
+router.post(
+  "/preview-upload-certificate",
+  previewUserUploadCertificateValidationEmail
+);
 
 module.exports = router;
