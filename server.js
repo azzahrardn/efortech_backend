9042a648e7766b0a20a45a648df444af8436b383
azzahrardn <115861841+azzahrardn@users.noreply.github.com>
2025-05-20 14:15:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const app = express();
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: [
+    'http://localhost:3000',
+    'https://efortech-edu-nu.vercel.app'
+  ]}));
 app.use(express.json());
 
 // Import & gunakan routes
